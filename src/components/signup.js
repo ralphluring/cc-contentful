998ba49447b74e11c81b65de79cee7995d4c2d21
Christopher <cb@Christophers-MacBook-Pro-2.local>
2019-12-31 01:08:52 -0800

    form fix 1

--- src/components/signup.js
+++ src/components/signup.js
@@ -4,7 +4,8 @@ import signupStyles from "./signupStyles.module.scss"
 const Signup = ({ open, setOpen }) => {
   return (
     <div className={signupStyles.container}>
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="signup" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="signup" />
         <div className={signupStyles.formbody}>
           <p className={signupStyles.name}>
             <label>
